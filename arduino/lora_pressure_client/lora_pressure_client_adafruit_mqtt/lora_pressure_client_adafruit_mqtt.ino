b837a2755375dea1717e72e9fcf99733a20c544d
daveMMbarker <davembarker@yahoo.com>
2025-08-08 13:48:57 -0400

    use flash to store credentials

--- arduino/lora_pressure_client/lora_pressure_client_adafruit_mqtt/lora_pressure_client_adafruit_mqtt.ino
+++ arduino/lora_pressure_client/lora_pressure_client_adafruit_mqtt/lora_pressure_client_adafruit_mqtt.ino
@@ -1,58 +1,36 @@
-
 // press button to return from deep sleep. I think this happens anyway without doing this
 #define HELTEC_POWER_BUTTON  // must be before "#include <heltec_unofficial.h>"
 #include <heltec_unofficial.h>
 #include <WiFi.h>
+#include <Preferences.h>
 
 #include "WiFiClientSecure.h"
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
-
 #include <ArduinoJson.h>
 
-// Frequency in MHz. Keep the decimal point to designate float.
-#define FREQUENCY 866.3  // for Europe
-// #define FREQUENCY           905.2       // for US
-
-// LoRa bandwidth. Keep the decimal point to designate float.
-// Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+/************************* Radio Setup *********************************/
+// Frequency in MHz
+#define FREQUENCY 866.3  // Europe
 #define BANDWIDTH 250.0
-
-// Number from 5 to 12. Higher means slower but higher "processor gain",
-// meaning (in nutshell) longer range and more robust against interference.
 #define SPREADING_FACTOR 9
-
-// only receiving, not transmitting
 #define TRANSMIT_POWER 0
 
 /************************* Adafruit.io Setup *********************************/
-
 #define AIO_SERVER      "io.adafruit.com"
-
-// Using port 8883 for MQTTS
 #define AIO_SERVERPORT  8883
 
-// Adafruit IO Account Configuration
-// (to obtain these values, visit https://io.adafruit.com and click on Active Key)
-#define AIO_USERNAME "davebarker"
-#define AIO_KEY      "aio_vvqR56Gh4a7FL8Pw4ezhnDiD36iE"
+Preferences prefs; // NVS storage
 
-const char* ssid = "ATTTtalCjA-Eero";
-const char* password = "ts3wubt?6j%e";
-
-/************ Global State (you don't need to change this!) ******************/
-// WiFiFlientSecure for SSL/TLS support
-WiFiClientSecure client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-#define WLAN_SSID "ATTTtalCjA-Eero"
-#define WLAN_PASS "ts3wubt?6j%e"
+String aio_username;
+String aio_key;
+String wifi;
+String wifi_pwd;
 
 // io.adafruit.com root CA
 const char* adafruitio_root_ca = \
-      "-----BEGIN CERTIFICATE-----\n"
-      "MIIEjTCCA3WgAwIBAgIQDQd4KhM/xvmlcpbhMf/ReTANBgkqhkiG9w0BAQsFADBh\n"
+"-----BEGIN CERTIFICATE-----\n"
+    "MIIEjTCCA3WgAwIBAgIQDQd4KhM/xvmlcpbhMf/ReTANBgkqhkiG9w0BAQsFADBh\n"
       "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
       "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"
       "MjAeFw0xNzExMDIxMjIzMzdaFw0yNzExMDIxMjIzMzdaMGAxCzAJBgNVBAYTAlVT\n"
@@ -77,26 +55,23 @@ const char* adafruitio_root_ca = \
       "jPuV6k2Cr/N3XxFpT3hRpt/3usU/Zb9wfKPtWpoznZ4/44c1p9rzFcZYrWkj3A+7\n"
       "TNBJE0GmP2fhXhP1D/XVfIW/h0yCJGEiV9Glm/uGOa3DXHlmbAcxSyCRraG+ZBkA\n"
       "7h4SeM6Y8l/7MBRpPCz6l8Y=\n"
-      "-----END CERTIFICATE-----\n";
+"-----END CERTIFICATE-----\n";
 
-/****************************** Feeds ***************************************/
-
-// Setup a feed called 'beargrass' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish beargrass = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/beargrass");
+/****************************** MQTT & Feeds *********************************/
+WiFiClientSecure client;
+Adafruit_MQTT_Client *mqtt; // We'll create this dynamically after loading creds
+Adafruit_MQTT_Publish *beargrass;
 
 JsonDocument doc;
-
-const int MAX_WORDS = 10; // Adjust as needed
+const int MAX_WORDS = 10;
 String words[MAX_WORDS]; 
-
 String rxdata;
 volatile bool rxFlag = false;
 
+/****************************** Setup Functions *********************************/
 void setupDisplay(void) {
   pinMode(Vext, OUTPUT);
   digitalWrite(Vext, LOW);
-  // Send a reset
   pinMode(RST_OLED, OUTPUT);
   digitalWrite(RST_OLED, HIGH);
   delay(1);
@@ -111,85 +86,108 @@ void setupDisplay(void) {
 
 void setupWiFi() {
   delay(10);
-
   WiFi.mode(WIFI_STA);
-  display.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
+  Serial.println(wifi);
+  WiFi.begin(wifi, wifi_pwd);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("\nWiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
 void setupRadio() {
   RADIOLIB_OR_HALT(radio.begin());
-  // Set the callback function for received packets
   radio.setDio1Action(rx);
-  // Set radio parameters
-  both.printf("Frequency: %.2f MHz\n", FREQUENCY);
   RADIOLIB_OR_HALT(radio.setFrequency(FREQUENCY));
-  both.printf("Bandwidth: %.1f kHz\n", BANDWIDTH);
   RADIOLIB_OR_HALT(radio.setBandwidth(BANDWIDTH));
-  both.printf("Spreading Factor: %i\n", SPREADING_FACTOR);
   RADIOLIB_OR_HALT(radio.setSpreadingFactor(SPREADING_FACTOR));
-  both.printf("TX power: %i dBm\n", TRANSMIT_POWER);
   RADIOLIB_OR_HALT(radio.setOutputPower(TRANSMIT_POWER));
-  // Start receiving
   RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
 }
 
+/****************************** Preferences *********************************/
+void loadAIOCredentials() {
+  prefs.begin("aio", false); // namespace "aio"
+  aio_username = prefs.getString("user", "");
+  aio_key      = prefs.getString("key", "");
+  prefs.end();
+
+  if (aio_username == "" || aio_key == "") {
+    // No stored creds — set defaults here or request via Serial
+    aio_username = "<aio username>";
+    aio_key = "<aio key";
+    
+    prefs.begin("aio", false);
+    prefs.putString("user", aio_username);
+    prefs.putString("key", aio_key);
+    prefs.end();
+    Serial.println("Stored default AIO credentials in NVS");
+  } else {
+    Serial.println("Loaded AIO credentials from NVS");
+  }
+  prefs.begin("wifi", false); // namespace "wifi"
+  wifi     = prefs.getString("wifi", "");
+  wifi_pwd = prefs.getString("wifi_pwd", "");
+  prefs.end();
+  if (wifi == "" || wifi_pwd == "") {
+    wifi = "<ssid>";
+    wifi_pwd = "<wifi pwd>";
+
+    prefs.begin("wifi", false);
+    prefs.putString("wifi", wifi);
+    prefs.putString("wifi_pwd", wifi_pwd);
+    prefs.end();
+    Serial.println("Stored default WIFI credentials in NVS");
+  } else {
+    Serial.println("Loaded WIFI credentials from NVS");
+  }
+}
+
+/****************************** Arduino Core *********************************/
 void setup() {
+  Serial.begin(115200);
   heltec_setup();
   setupDisplay();
   setupRadio();
+  loadAIOCredentials();
   setupWiFi();
   client.setCACert(adafruitio_root_ca);
+
+  mqtt = new Adafruit_MQTT_Client(&client, AIO_SERVER, AIO_SERVERPORT, aio_username.c_str(), aio_key.c_str());
+  beargrass = new Adafruit_MQTT_Publish(mqtt, (aio_username + "/feeds/beargrass").c_str());
 }
 
 void loop() {
   heltec_loop();
-
   MQTT_connect();
 
   if (rxFlag) {
     rxFlag = false;
     radio.readData(rxdata);
     if (_radiolib_status == RADIOLIB_ERR_NONE) {
-      heltec_led(50);  // flash LED upon receiving data
+      heltec_led(50);
       delay(100);
       heltec_led(0);
 
-      Serial.printf("RSSI[%.2f],SNR[%.2f]\n", radio.getRSSI(), radio.getSNR());
-      
-      // populates the global words
       splitString(rxdata.c_str(), " ");
-    
-      doc[String("id")] = words[1];
-      doc[String("timestamp")] = words[0];
-      //doc[String("temp")] = words[2];
-      //doc[String("bVolts")] = words[3];
-      doc[String("mA")] = words[4];
-      doc[String("RSSI")] = String(radio.getRSSI(), 2);
-      doc[String("SNR")] = String(radio.getSNR(), 2);
-      
+      doc["id"] = words[1];
+      doc["timestamp"] = words[0];
+      doc["mA"] = words[4];
+      doc["RSSI"] = String(radio.getRSSI(), 2);
+      doc["SNR"] = String(radio.getSNR(), 2);
+
       String output;
       serializeJson(doc, output);
 
-      if (! beargrass.publish(output.c_str())) {
+      if (!beargrass->publish(output.c_str())) {
         Serial.println(F("Failed"));
       } else {
         Serial.println(F("OK!"));
       }
-
       Serial.println(output);
       display.clear();
       display.drawStringMaxWidth(0, 0, 128, output);
@@ -198,16 +196,11 @@ void loop() {
   }
 }
 
-// not enough time to do anything during the interrupt but set a flag
-void rx() {
-  rxFlag = true;
-}
+void rx() { rxFlag = true; }
 
-// populates the global "words" String array
 int splitString(const char* str, char* delim) {
   int wordCount = 0;
-  char* token = strtok(const_cast<char*>(str), delim); // strtok modifies the string, so we need to cast away const
-
+  char* token = strtok(const_cast<char*>(str), delim);
   while (token != NULL && wordCount < MAX_WORDS) {
     words[wordCount++] = token;
     token = strtok(NULL, delim);
@@ -217,27 +210,19 @@ int splitString(const char* str, char* delim) {
 
 void MQTT_connect() {
   int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    return;
-  }
+  if (mqtt->connected()) return;
 
   Serial.print("Connecting to MQTT... ");
-
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
+  while ((ret = mqtt->connect()) != 0) {
+       Serial.println(mqtt->connectErrorString(ret));
        Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-        Serial.println("Unable to connect to MQTT");
-         //while (1);
+       mqtt->disconnect();
+       delay(5000);
+       if (--retries == 0) {
+         Serial.println("Unable to connect to MQTT");
+         return;
        }
   }
-
   Serial.println("MQTT Connected!");
 }
-
